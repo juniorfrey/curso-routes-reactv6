51d6ceaececd342f3ce97b6349bb14e0055ceb3a
Fredys Fernandez <juniorfrey2990@gmail.com>
2022-11-28 17:40:55 -0500

    Se agrega ejemplo de boton cerrar session.

--- login/src/pages/Home.jsx
+++ login/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 const Home = () => {
+    const cerrar = () => {
+        window.location.href = "./";
+    }
   return (
     <div>
         PAGE HOME
+        <button onClick={cerrar}>Cerrar</button>
     </div>
   )
 }
