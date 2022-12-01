6d037e8ef8a5004e1f6e5f4652dee6197a4bb1b6
Fredys Fernandez <juniorfrey2990@gmail.com>
2022-12-01 15:15:59 -0500

    Se le agrego estilos a las interfaces.

--- login/src/pages/Home.jsx
+++ login/src/pages/Home.jsx
@@ -12,11 +12,12 @@ const Home = () => {
 
     useEffect(() => {
       var user = sessionStorage.getItem("token_key");
+      console.log(!user);
+      if (!user) {
+        window.location.href = "./";
+      }
       getProyectos();
       return () => {
-        if (!user) {
-          window.location.href = "./";
-        }
       };
     }, []);
 
@@ -57,10 +58,26 @@ const Home = () => {
     }
   return (
     <div>
-      USUARIO {sessionStorage.getItem("user")}
+      <h3 style={{ color: "#26C6DA", fontFamily: "Arial" }}>
+        USUARIO: {sessionStorage.getItem("user") ? sessionStorage.getItem("user").toUpperCase() : ''}
+      </h3>
       <br />
       <br />
-      <button onClick={cerrar}>Cerrar</button>
+      <button
+        onClick={cerrar}
+        style={{
+          backgroundColor: "transparent",
+          border: "2px solid #F44336",
+          padding: "10px 10px 10px 10px",
+          width: "150px",
+          fontFamily: "Arial",
+          color: "#F44336",
+          borderRadius: "6px",
+          cursor: "pointer",
+        }}
+      >
+        CERRAR SESSIÓN
+      </button>
       <br />
       <br />
       <ul>
@@ -70,55 +87,96 @@ const Home = () => {
             style={{
               fontFamily: "Arial",
               textAlign: "left",
-              padding: "3px",
-              backgroundColor: "#f0f0f0",
+              padding: "10px 20px 10px 20px",
+              backgroundColor: "transparent",
 
-              border: "2px solid white",
-              borderBottom: "3px solid #FF5733",
+              border: "2px solid  #26C6DA",
+              borderBottom: "6px solid #26C6DA",
               borderRadius: "4px",
               listStyle: "none",
-              marginTop:'6px'
+              marginTop: "10px",
             }}
           >
-            <b style={{ color: "red" }}>{proyecto.nombre}</b>
-            <p>{proyecto.descripcion}</p>
-            <small>PRIORIDAD: {proyecto.prioridad}</small> <br />
+            <b style={{ color: " #26C6DA" }}>{proyecto.nombre}</b>
+            <p style={{ color: " white" }}>
+              {proyecto.descripcion.toUpperCase()}
+              <small
+                style={{
+                  color: " white",
+                  fontSize: "12px",
+                  marginLeft: "10px",
+                  color: "#26C6DA",
+                }}
+              >
+                PRIORIDAD: {proyecto.prioridad}
+              </small>
+            </p>
+            <hr style={{ border: "1px solid  #26C6DA" }} />
             <button
               style={{
-                background: "silver",
-                border: "2px solid green",
-                color: "green",
+                background: "#273746",
+                border: "2px solid #26C6DA",
+                color: "#26C6DA",
                 borderRadius: "5px",
+                padding: "8px",
+                boxSizing: "border-box",
+                margin: "3px 3px 3px 3px",
+                cursor: "pointer",
               }}
               onClick={() => verTareas(proyecto.id, proyecto.nombre)}
             >
               VER TAREAS
             </button>
+            <button
+              style={{
+                background: "transparent",
+                border: "2px solid #F44336",
+                color: "#26C6DA",
+                borderRadius: "5px",
+                padding: "8px",
+                boxSizing: "border-box",
+                margin: "3px 3px 3px 3px",
+                color: "#F44336",
+                float: "right",
+                cursor: "pointer",
+              }}
+              onClick={() => verTareas(proyecto.id, proyecto.nombre)}
+            >
+              ELIMINAR
+            </button>
           </li>
         ))}
       </ul>
       <br />
-      <label>
-        Tareas de <b>{nombreProyecto}</b>
+      <label style={{ color: "#26C6DA", fontFamily: "Arial" }}>
+        TAREAS - <b>{nombreProyecto.toUpperCase()}</b>
       </label>
-      <ul>
+      <ul style={{ display: "flex" }}>
         {tareas.map((tarea) => (
           <li
             key={tarea.id}
             style={{
-              border: "2px solid silver",
+              border: "2px solid #FF5733",
               fontFamily: "Arial",
-              textAlign: "left",
+              textAlign: "center",
               padding: "6px",
               borderRadius: "3px",
-              backgroundColor: "silver",
-              borderBottom: "1px solid #FF5733",
+              backgroundColor: "transparent",
               listStyle: "none",
+              width: "200px",
+              margin: "5px 10px 5px 0px",
             }}
           >
-            {tarea.nombre} <br />
-            <small style={{ color: "#FF5733", fontWeight: "bold" }}>
-              Fecha: {moment(tarea.createdAt).format("LLLL")}
+            <p
+              style={{ fontSize: "12px", color: "#CFD8DC", textAlign: "left" }}
+            >
+              {tarea.nombre}{" "}
+            </p>
+            <hr />
+            <small
+              style={{ color: "#FF5733", fontWeight: "bold", fontSize: "11px" }}
+            >
+              Fecha: {moment(tarea.createdAt).format("LLL")}
             </small>
           </li>
         ))}
@@ -128,3 +186,4 @@ const Home = () => {
 }
 
 export default Home
+
--- login/src/pages/Login.jsx
+++ login/src/pages/Login.jsx
@@ -52,47 +52,101 @@ const Login = () => {
 
   }
 
-  const setCampos = () => {
+  /* const setCampos = () => {
         setUsers({
           user: "fredys",
           password: "1234",
         });
-  }
+  } */
   
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>Usuario</label>
-        <br />
-        <input
-          type="text"
-          placeholder="Usuario"
-          onChange={(e) => setUsers({ ...users, user: e.target.value })}
-          value={users.user}
-          name="user"
-        />
-        <br />
-        <br />
-        <label>Password</label>
-        <br />
-        <input
-          type="text"
-          placeholder="Password"
-          onChange={(e) => setUsers({ ...users, password: e.target.value })}
-          value={users.password}
-          name="pass"
-        />
-        <br />
-        <br />
-        <br />
-        <button>Entrar</button>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          //border: "3px solid #F44336",
+          //backgroundColor: "#2E4053",
+          padding: "10px",
+          textAlign: "center",
+        }}
+      >
+        <label
+          style={{ color: "#1B4F72", fontFamily: "Arial", fontWeight: "bold" }}
+        >
+          INGRESA TUS CREDENCIALES
+        </label>
+        <hr />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "6px",
+            marginTop: "30px",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Usuario"
+            onChange={(e) => setUsers({ ...users, user: e.target.value })}
+            value={users.user}
+            name="user"
+            style={{
+              margin: "5px",
+              backgroundColor: "#1B4F72",
+              border: "3px solid #1B4F72",
+              width: "200px",
+              padding: "10px",
+              color: "#CFD8DC",
+              borderRadius: "5px",
+              fontSize: "15px",
+              fontWeight: "700",
+            }}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setUsers({ ...users, password: e.target.value })}
+            value={users.password}
+            name="pass"
+            style={{
+              margin: "5px",
+              backgroundColor: "#1B4F72",
+              border: "3px solid #1B4F72",
+              width: "200px",
+              padding: "10px",
+              color: "#CFD8DC",
+              borderRadius: "5px",
+              fontSize: "15px",
+              fontWeight: "700",
+            }}
+          />
+        </div>
+
+        <button
+          style={{
+            backgroundColor: "transparent",
+            border: "3px solid #1B4F72",
+            color: "#CFD8DC",
+            padding: "8px",
+            width: "460px",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          INGRESAR
+        </button>
       </form>
-      <button onClick={setCampos}>setCampos</button>
+      {/* <button onClick={setCampos} style={{backgroundColor:'transparent', border:'3px solid #1B4F72'}}>setCampos</button> */}
       {!mensaje ? (
-        <p style={{ color: "red" }}>Credenciales invalidas</p>
+        <p style={{ color: "red", fontFamily: "Arial", fontSize: "12px" }}>
+          Credenciales invalidas
+        </p>
       ) : (
-        <p style={{ color: "blue" }}>Credenciales correctas</p>
+        <p style={{ color: "#4CAF50", fontFamily: "Arial", fontSize: "12px" }}>
+          Credenciales correctas
+        </p>
       )}
     </div>
   );
